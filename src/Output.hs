a7416dc4e918f166c226d978a97e806d5cc7ad38
Julian Kotrba <me@juliankotrba.xyz>
2020-03-17 20:10:43 +0100

    Improve CLI UI

--- src/Output.hs
+++ src/Output.hs
@@ -17,9 +17,20 @@ drawGame game = let
     Nothing -> board game
   lineSize = length $ elems $ b!0
   list2d = chunksOf lineSize $ concat $ map elems $ elems b
-  in (unlines $ map lineToString list2d) ++ "\n"
+  width = boardWidth game
+  in topLeftCorner ++ (horizontalFieldSeparator width) ++ topRightCorner ++"\n" ++ 
+      (unlines $ map lineToString list2d) ++
+        bottomLeftCorner ++ (horizontalFieldSeparator width) ++ bottomRightCorner
 
 sideWall = [chr 0x2502]
+horizontalLine = [chr 0x2500]
+bottomLeftCorner = [chr 0x2514]
+bottomRightCorner = [chr 0x2518]
+topRightCorner = [chr 0x2510]
+topLeftCorner = [chr 0x250c]
+
+horizontalFieldSeparator :: Int -> String
+horizontalFieldSeparator boardWidth = concat $ replicate boardWidth horizontalLine
 
 lineToString :: [FieldState] -> String
 lineToString fss = sideWall ++ (intercalate "" $ map (\fs -> if fs == FULL then [chr 0x2588] else " ") fss) ++ sideWall
